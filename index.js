c52974f95a1af7747c2d0a3d231ece1c8d931692
haider <haideralimughal512@gmail.com>
2024-02-07 17:57:36 +0500

    backend and front combine

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
 require("dotenv").config()
-const cors = require("cors");
+const cors = require("cors")
 const bodyParser = require("body-parser")
-
+const path = require("path")
 const app = express()
 const port = process.env.port || 5500
 
@@ -10,8 +10,13 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get("/", (req, res) => {
+  const filePath = path.resolve(__dirname, "index.html")
+  res.sendFile(filePath)
+})
+
 app.listen(port, () => {
-  console.log(`server is listing on ${port} port`);
+  console.log(`server is listing on ${port} port`)
 })
 
 app.get("/api/:id", (req, res) => {
@@ -20,12 +25,19 @@ app.get("/api/:id", (req, res) => {
   const date = url.split("api/")
   if (url.includes("-")) {
     res.json({ unix: Date.parse(date[1]), utc: date[1] }).status(304)
-  }
-  else {
-    res.json({ unix: date[1], utc: new Date(parseInt(date[1])).toUTCString() }).status(304)
+  } else if (!url.includes("-")) {
+    res
+      .json({ unix: date[1], utc: new Date(parseInt(date[1])).toUTCString() })
+      .status(304)
   }
 })
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
+  debugger
+  const date = new Date().toUTCString()
+  res.json({ unix: Date.parse(date) }).status(304)
+})
+
+app.get("/alive", (req, res) => {
   res.send("server alive hellow world")
 })
